002840d54ca52d89e16117e7f4c1b25294c6bada
Rhustish <bbhatnagar27@gmail.com>
2022-09-25 19:49:35 +0530

    donefortheday

--- Web/JS/understandingreact/src/App.js
+++ Web/JS/understandingreact/src/App.js
@@ -1,14 +1,29 @@
+import { useState } from 'react';
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import Text from './components/Text';
 
 function App() {
+
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = "#0a0e11";
+    }else{
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  }
   return (
     <>
-    <Navbar/>
-    
-    <div className="container">
+    <Navbar title='Hello World' modex={`${mode}`} toggleMode = {toggleMode}/>
+    <div className="container" >
       <Text text = "Enter you text here"/>
+      {/* <About/> */}
+      
     </div>
     </>
   );
